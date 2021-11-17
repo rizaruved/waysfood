e584ae66af38dacf39a4f66512c50795ff78a81d
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-11-17 17:20:20 +0700

    / ‘src/pages/Home.jsx’

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
     return (
       <div className="home-banner">
           <div className="home-nav">
-              <div className="home-logo"><img src="./assets/png/waysfood.png"></div>
+              <div className="home-logo"><img src={"./assets/png/waysfood.png"}></div>
               <div>
                 <button className="home-button" style={{marginRight: 16, cursor: "pointer"}}>Register</button>
                 <button className="home-button" style={{marginRight: 93.94, cursor: "pointer"}}>Login</button>
