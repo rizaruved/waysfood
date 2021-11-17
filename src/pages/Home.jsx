545d5a88aab684e9383b49cea94bca8f34dc83ff
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-11-17 23:06:42 +0700

    / ‘src/App.css’ / ‘src/pages/Home.jsx’

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,17 +24,17 @@ function Home() {
           </div>
           
           {/* Jumbotron Content Wrapper */}
-          <div className="main">
-            <div className="mainText">
+          <div className="header">
+            <div className="headerText">
               <h1 >Are You Hungry? <br/> Express Home Delivery</h1>
-              <div className="mainContent">
-                <div className="mainStick"></div>
-                <div className="mainDescription">
+              <div className="headerContent">
+                <div className="headerStick"></div>
+                <div className="headerDescription">
                   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
                 </div>
               </div>
             </div>
-            <div className="mainImage">
+            <div className="headerImage">
               <img src={pizza} alt="pizza-image"/>
             </div>
           </div>
