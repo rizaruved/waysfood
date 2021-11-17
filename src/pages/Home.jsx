de3035d375564e5a3633bbc4193b70ecc32487a7
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-11-17 21:06:11 +0700

    / ‘src/pages/Home.jsx’

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,8 +26,10 @@ function Home() {
                 </div>
               </div>
             </div>
+            <div className="mainImage">
+              <img src={pizza} alt="pizza-image"/>
+            </div>
           </div>
-        <img src={pizza} alt="pizza-image" className="mainImage"/>
       </div>
     );
   }
