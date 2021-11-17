c3a2ba7f9fbff89d3ac9cdafe201bba6d322edea
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-11-17 20:58:03 +0700

    / ‘src/pages/Home.jsx’

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,31 +3,31 @@ import pizza from '../assets/png/pizza-icon.png';
 
 function Home() {
     return (
-      <div className="home-banner">
-          <div className="home-nav">
-              <div className="home-logo">
+      <div className="homeBanner">
+          <div className="homeNav">
+              <div className="homeLogo">
                 <img src={logo} alt="waysfood-logo" />
               </div>
               
               <div>
-                <button className="home-button" style={{marginRight: 16, cursor: "pointer"}}>Register</button>
-                <button className="home-button" style={{marginRight: 93.94, cursor: "pointer"}}>Login</button>
+                <button className="homeButton" style={{marginRight: 16, cursor: "pointer"}}>Register</button>
+                <button className="homeButton" style={{marginRight: 93.94, cursor: "pointer"}}>Login</button>
               </div>
           </div>
           
           <div className="main">
-            <div className="main-text">
+            <div className="mainText">
               <h1 id="main-text-1">Are You Hungry?</h1>
               <h1 id="main-text-2">Express Home Delivery</h1>
-              <div className="main-content">
-                <div className="main-stick"></div>
-                <div className="main-description">
+              <div className="mainContent">
+                <div className="mainStick"></div>
+                <div className="mainDescription">
                   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
                 </div>
               </div>
             </div>
             <div>
-              <img src={pizza} alt="pizza-image" className="main-image"/>
+              <img src={pizza} alt="pizza-image" className="mainImage"/>
             </div>
           </div>
       </div>
