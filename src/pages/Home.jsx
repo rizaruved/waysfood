7449cbe6f396b38a4be383db1616ad3f2b5ce9ee
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-11-17 17:06:47 +0700

    / ‘src/pages/Home.jsx’

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
               <p style={{marginTop: 0}}>WaysFood</p>
               <div>
                 <button className="home-button">Register</button>
-                <button className="home-button">Login</button>
+                <button className="home-button" style={{marginRight: 93.94}}>Login</button>
               </div>
           </div>
           <div>
